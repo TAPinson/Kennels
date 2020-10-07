c1b9778da59356803cf4571f615584b286a28507
Travis Pinson <travispinson@gmail.com>
2020-10-07 10:29:34 -0500

    Updated employee cards to show location via expanded fetch call

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -4,6 +4,6 @@ import "./Employee.css"
 export const EmployeeCard = (employee) => (
     <section className="employee">
         <h3 className="employee__name">{employee.name}</h3>
-        <div className="animal__location">{employee.location}</div>
+        <div className="animal__location">{employee.location.name}</div>
     </section>
 )
